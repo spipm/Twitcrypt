61d09a67a7bf84b29810a644a9307e1619b26087
DutchGraa <koffieprog@gmail.com>
2013-09-30 16:56:07 +0200

    Added hashing of password for cipher key. Added iterations for both hashing and encryption

--- twitcrypt/twitcrypt.py
+++ twitcrypt/twitcrypt.py
@@ -4,18 +4,43 @@
 	'''
 
 from Crypto.Cipher import Blowfish
+from hashlib import md5, sha1, sha224
 from getpass import getpass
 from os.path import isfile, isdir
 from os import mkdir
 
+
 # directory for keys
 d = 'twitkeys/'
+
+# global variables voor iterations
+iterations = 100
+baserounds = 100	# increase for better but slower encryption
+
 
 def getpassuser(line):
 	'''
 		Get user input for password
+		Set iterations based on password
+		Return hashed password
 		''' 
-	return getpass(line)
+	global iterations, baserounds
+
+	passkey = getpass(line)
+	l       = len(passkey)
+
+	# cipher iterations based on password length
+	iterations = l * baserounds
+
+	# hash the password
+	div 	= l / 2
+	passkey = passkey[:div] + md5(passkey).hexdigest() + passkey[div:]
+	for i in range(len(passkey)):
+		passkey = sha1(passkey).hexdigest()		
+	for i in range(iterations / (baserounds / 10)):
+		passkey = sha224(passkey).hexdigest()
+
+	return passkey
 
 def getapiuser():
 	'''
@@ -31,11 +56,15 @@ def padcrypt(crypt, key):
 	'''
 		Add padding and return some key encrypted
 		'''
+	global iterations
+	
 	l = 8 - len(key) % 8
 	if l < 8:	key = key + (chr(l) * l)
-	return crypt.encrypt(key)
+	
+	for i in range(iterations):	key = crypt.encrypt(key)
+	return key
 
-def newkeys(crypt, passkey):
+def newkeys(crypt):
 	'''
 		Save new API keys encrypted
 		'''
@@ -45,13 +74,20 @@ def newkeys(crypt, passkey):
 	open(d+'atkey.key',	'wb').write(padcrypt(crypt, atkey))
 	open(d+'atsecret.key',	'wb').write(padcrypt(crypt, atsecret))
 	return True
-	
+
 
 def stripchars(charkey):
 	'''
 		Strip padding characters
 		'''
 	return charkey.strip('\x01\x02\x03\x04\x05\x06\x07')
+
+def decryptkey(crypt, key):
+	'''
+		Decrypt encrypted key read from file
+		'''
+	for i in range(iterations):	key = crypt.decrypt(key)
+	return stripchars(key)
 
 def getapifiles(passkey):
 	'''
@@ -60,14 +96,16 @@ def getapifiles(passkey):
 	crypt = Blowfish.new(passkey)
 	if not isdir(d):
 		mkdir(d)
-		newkeys(crypt, passkey)
+		newkeys(crypt)
 	if not isfile(d+'ckey.key') or not isfile(d+'csecret.key') or not isfile(d+'atkey.key') or not isfile(d+'atsecret.key'):
-		newkeys(crypt, passkey)
-	ckey 	= stripchars(crypt.decrypt(open(d+'ckey.key',	'rb').read()))
-	csecret = stripchars(crypt.decrypt(open(d+'csecret.key','rb').read()))
-	atkey 	= stripchars(crypt.decrypt(open(d+'atkey.key',	'rb').read()))
-	atsecret= stripchars(crypt.decrypt(open(d+'atsecret.key','rb').read()))
+		newkeys(crypt)
+	ckey 	= decryptkey(crypt, open(d+'ckey.key',	'rb').read())
+	csecret = decryptkey(crypt, open(d+'csecret.key','rb').read())
+	atkey 	= decryptkey(crypt, open(d+'atkey.key',	'rb').read())
+	atsecret= decryptkey(crypt, open(d+'atsecret.key','rb').read())
 	return ckey, csecret, atkey, atsecret
+
+
 
 def getkeys():
 	'''
